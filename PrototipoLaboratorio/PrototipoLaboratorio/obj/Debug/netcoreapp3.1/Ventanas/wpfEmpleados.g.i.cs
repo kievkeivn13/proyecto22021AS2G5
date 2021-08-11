2603169629e543412d7ac7b9547f0d0b2a7bb068
Heydi Quemé <sucelyalvarez@hotmail.com>
2021-08-10 19:19:19 -0600

    actualizacion

--- PrototipoLaboratorio/PrototipoLaboratorio/obj/Debug/netcoreapp3.1/Ventanas/wpfEmpleados.g.i.cs
+++ PrototipoLaboratorio/PrototipoLaboratorio/obj/Debug/netcoreapp3.1/Ventanas/wpfEmpleados.g.i.cs
@@ -304,13 +304,13 @@ namespace PrototipoLaboratorio.Ventanas {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PrototipoLaboratorio;V1.0.0.0;component/ventanas/wpfempleados.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PrototipoLaboratorio;component/ventanas/wpfempleados.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Ventanas\wpfEmpleados.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -320,7 +320,7 @@ namespace PrototipoLaboratorio.Ventanas {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- PrototipoLaboratorio/PrototipoLaboratorio/obj/Debug/netcoreapp3.1/Ventanas/wpfRequerimientosClinica.g.i.cs
+++ PrototipoLaboratorio/PrototipoLaboratorio/obj/Debug/netcoreapp3.1/Ventanas/wpfRequerimientosClinica.g.i.cs
@@ -128,13 +128,13 @@ namespace PrototipoLaboratorio.Ventanas {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PrototipoLaboratorio;V1.0.0.0;component/ventanas/wpfrequerimientosclinica.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PrototipoLaboratorio;component/ventanas/wpfrequerimientosclinica.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Ventanas\wpfRequerimientosClinica.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -144,7 +144,7 @@ namespace PrototipoLaboratorio.Ventanas {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
